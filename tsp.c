ee38da0005c8c70294568b4553c66d09861694aa
nyee32 <nyee32@gmail.com>
2016-02-14 22:12:21 -0800

    created array of airport structs

--- tsp.c
+++ tsp.c
@@ -8,7 +8,8 @@ void parseAirports(char *fileName) {
    FILE *fp = fopen(fileName, "r");
    char buf[256];
    char *token;
-   Airport a; 
+   Airport *allAirport = malloc(sizeof(Airport) * 10000);
+   int cnt = 0;
 
    if(fp == NULL) {
       printf("Error opening airports file");
@@ -17,21 +18,45 @@ void parseAirports(char *fileName) {
 
    while(fgets(buf, sizeof(buf), fp)) {
       token = strtok(buf, ",");
-      while(token != NULL) {
-         printf("%s\n", token);     
-         token = strtok(NULL, ",");
-      }
+
+      // Must free all airport structs and the strings
+      Airport a = {strtol(token, (char **)NULL, 10), strdup(strtok(NULL, ",")),
+		   strdup(strtok(NULL, ",")), strdup(strtok(NULL, ",")),
+		   strdup(strtok(NULL, ",")), strdup(strtok(NULL, ",")),
+		   strtod(strtok(NULL, ","),(char **)NULL), // lat
+		   strtod(strtok(NULL, ","), (char **)NULL), // long
+		   strtol(strtok(NULL, ","), (char **)NULL, 10), // alt
+		   strtol(strtok(NULL, ","), (char **)NULL, 10), // timezone
+		   strtol(strtok(NULL, ","), (char **)NULL, 10),
+		   strtok(NULL, ",")};
+
+	      allAirport[cnt] = a;
+	      cnt++;
    }
+
+   printf ("\n\n\nnumber of airports = %d\n",cnt);
+
+   for (int i = 0; i < cnt; i++) {
+	   printf ("\n=========================================\n");
+	   printf ("id = %d\n",allAirport[i].airportID);
+	   printf ("name = %s\n",allAirport[i].name);
+	   printf ("city = %s\n",allAirport[i].city);
+	   printf ("country = %s\n",allAirport[i].country);
+	   printf ("airport code = %s\n",allAirport[i].airportCode);
+	   printf ("lat = %lf\n",allAirport[i].latitude);
+	   printf ("long = %lf\n",allAirport[i].longitude);
+   }
+
 
    fclose(fp);
 }
-   
+
 
 
 
 int main(int argc, char **argv) {
-   
-  parseAirports("airports.dat");  
+
+  parseAirports("airports.dat");
 
 
 }
